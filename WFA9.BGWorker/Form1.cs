2b76b3cabefe72a7c46c2f677096450f3d7ec12a
linlijian <praw__rul2__lvluk@outlook.co.th>
2021-11-23 21:50:42 +0700

    backgroundWorker

---
+++ WFA9.BGWorker/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA9.BGWorker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int i = 0;
+            while (true)
+            {
+                if (backgroundWorker1.CancellationPending)
+                    break;
+                Debug.WriteLine(i);
+                i++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (true)
+            {
+                if (i == 10000)
+                    break;
+                Debug.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
